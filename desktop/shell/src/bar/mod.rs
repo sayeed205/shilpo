64a5ca46aa55d71c87bd61a3b14fc0caae96aebd
hitarashi <sayeed205@gmail.com>
2026-08-06 21:45:16 +0530

    refactor(shell): inline BarState back into BarView (#12)

--- desktop/shell/src/bar/mod.rs
+++ desktop/shell/src/bar/mod.rs
@@ -2,10 +2,9 @@ pub mod ext_view_adapter;
 pub mod geometry;
 pub mod reconciliation;
 pub mod service_worker;
-pub mod state;
 pub mod view;
 pub mod widgets;
 
 pub use reconciliation::{BarSpec, OutputDescriptor, ReconciliationOp, reconcile_output_bars};
-pub use state::{BarState, BarStateEffect, BarUpdateResult};
 pub use view::BarView;
+
