d5fe19c6b9e9534bb8b586214f3c4ec2e9f2f48b
hitarashi <sayeed205@gmail.com>
2026-08-09 10:00:19 +0530

    fix installer device daemon deployment

--- scripts/install/utils.sh
+++ scripts/install/utils.sh
@@ -10,6 +10,7 @@ error() { printf 'error: %s\n' "$*" >&2; }
 SHILPO_USER_UNITS=(
   shilpo-shell.service
   shilpo-themed.service
+  shilpo-device-daemon.service
   shilpo-wallpaper.service
   shilpo-polkit-agent.service
   shilpo-network-agent.service
@@ -21,6 +22,7 @@ SHILPO_USER_UNITS=(
 SHILPO_SESSION_UNITS=(
   shilpo-shell.service
   shilpo-themed.service
+  shilpo-device-daemon.service
   shilpo-wallpaper.service
   shilpo-polkit-agent.service
   shilpo-network-agent.service
